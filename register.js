7d0242faec4c07acb2512d96e76e179c2ce43d06
ThanachatAim <Thanachat.aim@gmail.com>
2023-08-17 22:24:07 +0700

    assignment 2

---
+++ register.js
@@ -0,0 +1,22 @@
+function check() {
+    if (document.getElementById("company_name").value==""){
+        alert("Empty Company Name isn't allow")
+    } 
+    else if (document.getElementById("contract_name").value==""){
+        alert("Empty Contract Name isn't allow")
+    }
+    else if (document.getElementById("contract_phone").value==""){
+        alert("Empty Contract Phone isn't allow")
+    }
+    else if (document.getElementById("boot_size").value==""){
+        alert("Empty Boot Size isn't allow")
+    }
+    else if (document.getElementById("chair").value < 1 || document.getElementById("chair").value > 10){
+        alert("Input must be 1-10")
+    }
+    else {
+        alert("OK")
+    }
+    
+}
+
