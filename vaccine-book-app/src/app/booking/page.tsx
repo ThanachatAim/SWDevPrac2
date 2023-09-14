069b44e07feec75771a3bf8357109504779965c0
Thanachat <thanachat.aim@gmail.com>
2023-09-14 21:25:38 +0700

    assignment 5

--- vaccine-book-app/src/app/booking/page.tsx
+++ vaccine-book-app/src/app/booking/page.tsx
@@ -1,7 +1,52 @@
+import LocationDateReserve from "@/components/LocationDateReserve"
 export default function Booking() {
     return (
-        <main>
-            <div className="font-sans m-5">Booking</div>
+        <main className="w-[100%] flex flex-col items-center space-y-4">
+            <div className="font-sans text-xl mt-2 font-bold">Booking</div>
+            
+            <div className="w-fit space-y-2 rounded border-slate-400">
+                <label htmlFor="name" className="block text-sm font-medium leading-6 text-gray-900">
+                Name
+                </label>
+                <input
+                    type="text"
+                    name="name"
+                    id="name"
+                    autoComplete="name"
+                    className="rounded ring-1 ring-inset flex-1 border-1 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
+                    placeholder="สมชาย"
+                  />
+                <label htmlFor="surname" className="block text-sm font-medium leading-6 text-gray-900">
+                Surname
+                </label>
+                <input
+                    type="text"
+                    name="surname"
+                    id="surname"
+                    autoComplete="surname"
+                    className="rounded ring-1 ring-inset flex-1 border-1 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
+                    placeholder="ใจดี"
+                  />
+                <label htmlFor="surname" className="block text-sm font-medium leading-6 text-gray-900">
+                Citizen ID
+                </label>
+                <input
+                    type="text"
+                    name="surname"
+                    id="surname"
+                    autoComplete="surname"
+                    className="rounded ring-1 ring-inset flex-1 border-1 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
+                    placeholder="1234567890000"
+                  />
+                <div className="text-md text-left text-gray-600">Select Date and Location</div>
+                <LocationDateReserve/>
+            </div>
+
+            <button className="block rounded-md bg-sky-600 hover:bg-indigo-600 px-3 py-2
+            text-white shadow-sm">
+                Book Vaccine Center
+            </button>
+            
         </main>
     )
 }
--- vaccine-book-app/src/components/ProductCard.tsx
+++ vaccine-book-app/src/components/ProductCard.tsx
@@ -1,9 +1,14 @@
-import styles from './productcard.module.css'
 import Image from 'next/image';
+import InteractiveCard from './InteractiveCard';
 
 export default function ProductCard({vacName,imgSrc}:{vacName:string, imgSrc:string}) {
+
+    function onVacSelected(){
+        alert("You Select " + vacName)
+    }
+
     return (
-        <div className='w-1/5 h-[300px] rounded-lg shadow-lg bg-gray-100'>
+        <InteractiveCard contentName={vacName}>
             <div className='w-full h-[70%] relative rounded-t-lg'>
                 <Image src={imgSrc}
                 alt='Product Picture'
@@ -12,6 +17,6 @@ export default function ProductCard({vacName,imgSrc}:{vacName:string, imgSrc:str
                 />
             </div>
             <div className='w-full h-30 p-[10px] font-kanit'>{vacName}</div>
-        </div>
+        </InteractiveCard>
     );
 }
