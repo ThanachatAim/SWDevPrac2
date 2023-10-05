4e23205e4f3fb391c38bae8bab622265777271ae
ThanachatAim <Thanachat.aim@gmail.com>
2023-10-05 22:17:50 +0700

    assignment 8

--- vaccine-book-app/src/app/page.tsx
+++ vaccine-book-app/src/app/page.tsx
@@ -1,8 +1,6 @@
-import Image from 'next/image'
+import { PromoteCard } from '@/components/PromoteCard'
 import Banner from '@/components/Banner'
-import styles from './page.module.css'
-import ProductCard from '@/components/ProductCard'
-import CardPanel from '@/components/CardPanel'
+
 
 export default function Home() {
   return (
@@ -10,6 +8,7 @@ export default function Home() {
       <link rel="stylesheet" 
       href="https://fonts.googleapis.com/css2?family=Kanit:wght@200;300;400;500;600;700;800&display=swap"/>
       <Banner/>
+      <PromoteCard/>
     </main>
   )
 }
