0844eb92ccfd5e09a2c6c127bb342104d7954bc3
ThanachatAim <Thanachat.aim@gmail.com>
2023-10-18 09:28:19 +0700

    assignment 10

--- vaccine-book-app/src/app/booking/page.tsx
+++ vaccine-book-app/src/app/booking/page.tsx
@@ -1,9 +1,25 @@
 import LocationDateReserve from "@/components/LocationDateReserve"
-export default function Booking() {
+import { getServerSession } from "next-auth"
+import { authOptions } from "../api/auth/[...nextauth]/route"
+import getUserProfile from "@/libs/getUserProfile"
+
+export default async function Booking() {
+
+    const session = await getServerSession(authOptions)
+    if(!session || !session.user.token) return null
+
+    const profile = await getUserProfile(session.user.token)
+    var createdAt = new Date(profile.data.createdAt)
+
     return (
         <main className="w-[100%] flex flex-col items-center space-y-4">
+            <div className="text-2xl mt-5">{profile.data.name}</div>
+            <table className="table-auto border-separate border-spacing-2"><tbody>
+                <tr><td>Email</td><td>{profile.data.email}</td></tr>
+                <tr><td>Tel</td><td>{profile.data.tel}</td></tr>
+                <tr><td>Member Since</td><td>{createdAt.toString()}</td></tr>
+            </tbody></table>
             <div className="font-sans text-xl mt-2 font-bold">Booking</div>
-            
             <div className="w-fit space-y-2 rounded border-slate-400">
                 <label htmlFor="name" className="block text-sm font-medium leading-6 text-gray-900">
                 Name
--- vaccine-book-app/src/components/Banner.tsx
+++ vaccine-book-app/src/components/Banner.tsx
@@ -3,11 +3,15 @@ import {useState} from 'react';
 import { useRouter } from 'next/navigation';
 import styles from './banner.module.css'
 import Image from 'next/image';
+import {useSession} from 'next-auth/react'
 
 export default function Banner() {
     const covers = ['/img/cover.jpg','/img/cover2.jpg','/img/cover3.jpg','/img/cover4.jpg']
     const [index, setIndex] = useState(0)
     const router = useRouter()
+
+    const {data: session} = useSession()
+    console.log(session?.user.token)
 
     return(
         <div className="block p-5 m-0 w-full h-96 relative" onClick={()=>{setIndex(index+1)}}>
@@ -21,6 +25,11 @@ export default function Banner() {
                 <h1 className='text-6xl font-bold font-sans bg-slate-200 inline p-3 rounded-2xl border-dashed'>Vaccine Center</h1><br></br><br></br>
                 <h3 className='text-4xl font-medium font-kanit bg-slate-200 inline p-3 rounded-2xl border-dashed'>ศูนย์บริการวัคซีน</h3>
             </div>
+            {
+                session? <div className='z-30 absolute top-5 right-10 font-semibold text-xl'>
+                        Hello {session.user?.name}</div>
+                        :null
+            }
             <button className='bg-white text-cyan-600 border border-cyan-600
             font-semibold py-2 px-2 m-2 rounded z-30 absolute bottom-0 right-0
             hover:bg-cyan-600 hover:text-white hover:border-transparent'
